555ad26ba0cfea3705180b20915a31acf161d33f
cristiane silva <criscage@hotmail.com>
2020-08-20 16:57:34 -0300

    entrando em views

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,14 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+end
