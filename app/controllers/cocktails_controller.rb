bb4b8b0215d2ab1030197dddf49924e4d8ecc119
cristiane silva <criscage@hotmail.com>
2020-08-21 16:06:47 -0300

    finish show flow

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,13 @@
 class CocktailsController < ApplicationController
 
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def new
     @cocktail = Cocktail.new
   end
@@ -13,13 +21,7 @@ class CocktailsController < ApplicationController
     end
   end
 
-  def index
-    @cocktails = Cocktail.all
-  end
 
-  def show
-    @dose = Dose.new
-  end
 
   private
 
